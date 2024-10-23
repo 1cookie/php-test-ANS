e25687534837d982c3b575f005ade6e4726377fa
Andy C <andy.cookson@nexusrental.co.uk>
2024-10-23 13:13:04 +0100

    inject the PokemonService and call the API to get the list of Pokemon

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Services\PokemonService;
 
 class PokemonController extends Controller
 {
-    //
+    protected PokemonService $pokemonService;
+
+    public function __construct(PokemonService $pokemonService)
+    {
+        $this->pokemonService = $pokemonService;
+    }
+
+    public function index()
+    {
+        $pokemons = $this->pokemonService->getAllPokemons();
+        return view('pokemons.index', compact('pokemons'));
+    }
+
+    public function show($name)
+    {
+        $pokemon = $this->pokemonService->getPokemonByName($name);
+        return view('pokemons.show', compact('pokemon'));
+    }
 }
